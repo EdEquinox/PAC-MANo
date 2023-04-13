c4c25709f5c98fbecf9744d3cbac23d01c4ee1b9
EdEquinox <zemarquez97@hotmail.com>
2023-04-13 15:14:27 +0100

    nao fiz grande coisa mas giro

--- src/pt/isec/pa/tinypac/Main.java
+++ src/pt/isec/pa/tinypac/Main.java
@@ -1,7 +1,10 @@
 package pt.isec.pa.tinypac;
 
+import pt.isec.pa.tinypac.ui.text.PacManTUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PacManTUI pacManTUI = new PacManTUI();
+        pacManTUI.start();
     }
 }
