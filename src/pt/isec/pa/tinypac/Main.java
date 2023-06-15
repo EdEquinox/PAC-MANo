30f8a6c93920c905fd81bc5f441e73592d802d0b
EdEquinox <47253077+EdEquinox@users.noreply.github.com>
2023-06-15 14:02:36 +0100

    para duvidas

--- src/pt/isec/pa/tinypac/Main.java
+++ src/pt/isec/pa/tinypac/Main.java
@@ -5,8 +5,17 @@ import pt.isec.pa.tinypac.model.PacmanManager;
 import pt.isec.pa.tinypac.ui.gui.MainJFX;
 
 import java.io.IOException;
-
+/**
+ Main class
+ *
+ * The first class of this project
+ *
+ * @author Jose Marques
+ * @version 1.0.0
+ *
+ */
 public class Main {
+
 
     public static PacmanManager manager;
     public static void main(String[] args) throws IOException {
@@ -16,6 +25,8 @@ public class Main {
 
     //TODO como é que paro o game engine porque quando mudo de nivel dá raia
     //TODO pacman para no lunch
+    //TODO testes fazem se no context?
+    //todo o que por no javadoc
     //TODO tests
     //TODO doc
 }
